7a66eee2f61bc088309bcc530f049e89b686657c
Lukau Garcia <lukau.dev@gmail.com>
2023-11-09 16:04:19 +0100

    Add array empty validation on max and min

--- src/array.ts
+++ src/array.ts
@@ -1,9 +1,13 @@
-const min = (arr: Array<number>): number => {
+const min = (arr: Array<number>): number | string => {
+    if (isArrayEmpty(arr)) return 'The array must not be empty';
+
     arr.sort((a: number, b: number) => (a - b));
     return arr[0];
 }
 
-const max = (arr: Array<number>): number => {
+const max = (arr: Array<number>): number | string => {
+    if (isArrayEmpty(arr)) return 'The array must not be empty';
+
     arr.sort((a: number, b: number) => (b - a));
     return arr[0];
 }
